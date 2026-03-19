2f179408955eef4adb01271122a7e624f9a911a7
BOOBESH776 <cboobesh776@gmail.com>
2026-03-19 18:31:38 +0530

    successfully create a country name in option

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ let api = fetch('https://api.frankfurter.dev/v1/latest')
 function dropdown(data) {
     let currency = Object.entries(data);
     console.log(currency);
-    // for(let i=0;i<=currency.length;i++){
-    //     currency +=`<option value="${currency[i][0]}">${currency[i][0]}</option>`;
-    // }
+    let option = "";
+    for (let i = 0; i < currency.length; i++) {
+        option += `<option value="${currency[i][0]}">${currency[i][0]}</option>`;
+    }
+    let select = document.getElementById("currency").innerHTML = option;
+    let select_2 = document.getElementById("currency_2").innerHTML = option;
 }
