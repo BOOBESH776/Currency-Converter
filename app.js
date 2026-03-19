d34b4beaabbba78569df84251480ca1d3249f03c
BOOBESH776 <cboobesh776@gmail.com>
2026-03-19 18:14:59 +0530

    use for loop to display the country

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 let api = fetch('https://api.frankfurter.dev/v1/latest')
+    .then((res) => res.json())   // convert response to JSON
+    .then((data) => {
+        dropdown(data.rates);      // pass only rates object
+    });
 
-.then((res) => console.log(res.json()))
-.then
+function dropdown(data) {
+    let currency = Object.entries(data);
+    console.log(currency);
+    // for(let i=0;i<=currency.length;i++){
+    //     currency +=`<option value="${currency[i][0]}">${currency[i][0]}</option>`;
+    // }
+}
